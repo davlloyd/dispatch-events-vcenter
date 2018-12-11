0bfe3fc8ea719d066a2bcfeb0635a4ceeb86d8e9
davlloyd <davidlloyd0@gmail.com>
2018-12-11 19:45:07 +1100

    Added more explicit events

--- handlers.go
+++ handlers.go
@@ -25,7 +25,27 @@ func processEventMetadata(e types.BaseEvent) interface{} {
 		return handleVMPoweredOnEvent(concreteEvent)
 	case *types.VmPoweredOffEvent:
 		return handleVMPoweredOffEvent(concreteEvent)
+	case *types.VmCloneEvent:
+		return handleVMCloneEvent(concreteEvent)
+	case *types.VmDeployFailedEvent:
+		return handleVMDeployFailedEvent(concreteEvent)
 	case *types.VmEvent:
+		return handleVMEvent(concreteEvent)
+	case *types.VmFailedToPowerOffEvent:
+		return handleVMFailedToPowerOffEvent(concreteEvent)
+	case *types.VmFailedToPowerOnEvent:
+		return handleVMFailedToPowerOnEvent(concreteEvent)
+	case *types.VmFailedToRebootGuestEvent:
+		return handleVMFailedToRebootGuestEvent(concreteEvent)
+	case *types.VmFailedToResetEvent:
+		return handleVMFailedToResetEvent(concreteEvent)
+	case *types.VmFailedToShutdownGuestEvent:
+		return handleVMFailedToShutdownGuestEvent(concreteEvent)
+	case *types.VmFailedToStandbyGuestEvent:
+		return handleVMFailedToStandbyGuestEvent(concreteEvent)
+	case *types.VmFailedToSuspendEvent:
+		return handleVMFailedToSuspendEvent(concreteEvent)
+	case *types.VmFailoverFailed:
 		return handleVMEvent(concreteEvent)
 	default:
 		return handleUnknownEvent(reflect.TypeOf(e).String())
@@ -80,6 +100,26 @@ func handleVMEvent(e *types.VmEvent) interface{} {
 	}
 }
 
+func handleVMCloneEvent(e *types.VmEvent) interface{} {
+	return struct {
+		VMName     string `json:"vm_name"`
+		VMID       string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
+func handleVMDeployFailedEvent(e *types.VmEvent) interface{} {
+	return struct {
+		VMName     string `json:"vm_name"`
+		VMID       string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
 func handleVMPoweredOnEvent(e *types.VmPoweredOnEvent) interface{} {
 	return struct {
 		VMName     string `json:"vm_name"`
@@ -100,6 +140,79 @@ func handleVMPoweredOffEvent(e *types.VmPoweredOffEvent) interface{} {
 	}
 }
 
+// Failure Events
+
+
+func handleVMFailedToPowerOffEvent(e *types.VmPoweredOffEvent) interface{} {
+	return struct {
+		VMName     	string `json:"vm_name"`
+		VMID		string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
+func handleVMFailedToPowerOnEvent(e *types.VmPoweredOffEvent) interface{} {
+	return struct {
+		VMName     	string `json:"vm_name"`
+		VMID		string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+func handleVMFailedToRebootGuestEvent(e *types.VmPoweredOffEvent) interface{} {
+	return struct {
+		VMName     	string `json:"vm_name"`
+		VMID		string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
+func handleVMFailedToResetEvent(e *types.VmPoweredOffEvent) interface{} {
+	return struct {
+		VMName     	string `json:"vm_name"`
+		VMID		string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
+func handleVMFailedToSuspendEvent(e *types.VmPoweredOffEvent) interface{} {
+	return struct {
+		VMName     	string `json:"vm_name"`
+		VMID		string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
+func handleVMFailedToShutdownGuestEvent(e *types.VmPoweredOffEvent) interface{} {
+	return struct {
+		VMName     	string `json:"vm_name"`
+		VMID		string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
+func handleVMFailedToStandbyGuestEvent(e *types.VmPoweredOffEvent) interface{} {
+	return struct {
+		VMName     	string `json:"vm_name"`
+		VMID		string `json:"vm_id"`
+	}{
+		VMName:     e.Vm.Name,
+		VMID:       e.Vm.Vm.String(),
+	}
+}
+
+// Catch-all event
 func handleUnknownEvent(eventtype string) interface{} {
 	return struct {
 		EventType	string `json:"eventtype"`
@@ -109,3 +222,5 @@ func handleUnknownEvent(eventtype string) interface{} {
 		Comment:    "Event Type not declared",
 	}
 }
+
+
